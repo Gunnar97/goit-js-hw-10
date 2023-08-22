aca367515d3622230bc047178ef5e7b9372a8b90
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-08-22 21:17:14 +0300

    add finish

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,10 +1,11 @@
+import Notiflix from 'notiflix'; 
 const BASE_URL = 'https://api.thecatapi.com';
-const END_POINT = '/v1/breeds/';
+const END_POINT = '/v1/';
 const API_KEY = 'live_ohr4TroEeALFAeX9AwVTyht5ME5G5GQHIqILsL5OUq5KibI4yr7Dg55ykvZJ2zYe';
+const url = `${BASE_URL}${END_POINT}breeds/`;
+const  loader = document.querySelector('.loader')
 
-function fetchBreeds() {
-
-  const url = `${BASE_URL}${END_POINT}`;
+export function fetchBreeds() {
  return fetch(url,{headers: {
       'x-api-key': API_KEY
     }}).then(res => {
@@ -13,13 +14,35 @@ function fetchBreeds() {
     } else {
       throw new Error('Error');
     }
-    })
+    })  .catch(error => {
+       Notiflix.Report.failure (
+                'ERROR',
+                'Oops! Something went wrong! Try reloading the page!',
+         'Okay',);
+      loader.classList.add('ishidden');
+        });
 };
 
 
+   
+export function fetchCatByBreed(id) {
 
+  return fetch(`${BASE_URL}${END_POINT}images/search?breed_ids=${id}`,
+    {
+      headers: {
+      'x-api-key': API_KEY
+    }}).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error('Error');
+    }
+    }) .catch(error => {
+       Notiflix.Report.failure (
+                'ERROR',
+                'Oops! Something went wrong! Try reloading the page!',
+         'Okay',);
+      loader.classList.add('ishidden');
+        });
 
-
-  
-
-
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,52 @@
 import SlimSelect from 'slim-select';
-import { fetchBreeds } from './cat-api';
+import 'slim-select/dist/slimselect.css';
+import { fetchBreeds } from './cat-api.js';
+import { fetchCatByBreed } from './cat-api.js'
 const refs = {
     single: document.querySelector('.breed-select'),
     loader: document.querySelector('.loader'),
+    cat: document.querySelector('.cat-info'),
+    error: document.querySelector('.error')
 }
 
 
-// function templateSelect({name, reference_image_id}) {
+refs.single.classList.add('ishidden');
+refs.error.classList.add('ishidden');
+fetchBreeds().then(data => {
     
-// };
+    renderList(data)
+    refs.loader.classList.add('ishidden')
+});
 
 
+function renderList(arr) {
+  const listBreeds = arr.map(breed => `<option  value="${breed.id}"><span>${breed.name}</span></option>`).join('');
+    refs.single.innerHTML = listBreeds;
+    new SlimSelect({
+    select: refs.single,
+})
+};
+
+
+refs.single.addEventListener('change', onChange);
+
+function onChange(eve) {
+    refs.cat.innerHTML = '';
+     refs.loader.classList.remove('ishidden')
+    const selectedBreedId = eve.target.value;
+    fetchCatByBreed(selectedBreedId).then(data => {
+        renderCat(data);
+        refs.loader.classList.add('ishidden')
+    })
+}
+
+function renderCat(arr) {
+  const catByBreeds = arr.map(breed => `<img src="${breed.url
+      }" alt="${breed.breeds[0].name}" width="600px">
+<div class="info"><h2>${breed.breeds[0].name}</h2>
+    <p class="description">${breed.breeds[0].description}</p>
+    <p><span class="temperament">Temperament:</span> ${breed.breeds[0].temperament}</p></div>
+    `).join('');
+    refs.cat.innerHTML = catByBreeds;
+};
 
